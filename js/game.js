7d6e8f58f92ba1a4771cf67e9e2b30011f90d26a
Paul Thoren <pthoren@palantir.com>
2013-08-18 16:54:11 -0700

    Add canvas

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+var tox = tox || {};
+
+tox.WIDTH = 800;
+tox.HEIGHT = 600;
+
+$(function() {
+  tox.init();
+  tox.ctx.fillStyle = 'green';
+  tox.ctx.fillRect(0, 0, tox.WIDTH, tox.HEIGHT);
+});
+
+tox.init = function() {
+  tox.canvas = $('#gamecanvas').get(0);
+  tox.ctx = tox.canvas.getContext('2d');
+  tox.canvas.width = tox.WIDTH;
+  tox.canvas.height = tox.HEIGHT;
+};
+
+console.log(tox);
