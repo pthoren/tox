76e675f6a1ca819f40740349806f4d3b9bfd1d98
Paul Thoren <pthoren@palantir.com>
2013-08-18 17:56:52 -0700

    Add controller

---
+++ js/controller.js
@@ -0,0 +1,59 @@
+tox.Controller = function() {
+  this.mouse = {
+    x: 0,
+    y: 0,
+    leftPressed: false,
+    rightPressed: false
+  };
+
+  this.bindings = {
+    spell0: 65, //a
+    spell1: 83, //s
+    spell2: 68, //d
+    spell3: 70, //f
+    jump: 32 //space
+  };
+};
+
+tox.Controller.prototype.init = function() {
+  tox.keysPressed = {};
+
+  var _this = this;
+
+  tox.canvas.onmousemove = function(e) {
+    _this.mouse.x = e.x;
+    _this.mouse.y = e.y;
+    console.log(_this.mouse.leftPressed, _this.mouse.rightPressed);
+  };
+
+  tox.canvas.onmousedown = function(e) {
+    if (e.button === 0) {
+      _this.mouse.leftPressed = true;
+    } else if (e.button === 2) {
+      _this.mouse.rightPressed = true;
+    }
+  }
+
+  tox.canvas.onmouseup = function(e) {
+    if (e.button === 0) {
+      _this.mouse.leftPressed = false;
+    } else if (e.button === 2) {
+      _this.mouse.rightPressed = false;
+    }
+  }
+
+  window.addEventListener("keydown", function(e) {
+    console.log(e.keyCode);
+    tox.keysPressed[e.keyCode] = true;
+  }, true);
+
+  window.addEventListener("keyup", function(e) {
+    tox.keysPressed[e.keyCode] = false;
+  }, true);
+};
+
+tox.Controller.prototype.isPressed = function(keyName) {
+  return tox.keysPressed[this.bindings[keyName]]; 
+};
+
+
--- js/game.js
+++ js/game.js
@@ -1,19 +1,31 @@
 var tox = tox || {};
 
+var States = {
+  MENU: 0,
+  PLAY: 1
+}
+
 tox.WIDTH = 800;
 tox.HEIGHT = 600;
 
 $(function() {
   tox.init();
-  tox.ctx.fillStyle = 'green';
-  tox.ctx.fillRect(0, 0, tox.WIDTH, tox.HEIGHT);
 });
 
 tox.init = function() {
+  tox.createCanvas();
+  tox.state = States.PLAY;
+};
+
+tox.createCanvas = function() {
   tox.canvas = $('#gamecanvas').get(0);
   tox.ctx = tox.canvas.getContext('2d');
   tox.canvas.width = tox.WIDTH;
   tox.canvas.height = tox.HEIGHT;
+  tox.ctx.fillStyle = '#000000';
+  tox.ctx.fillRect(0, 0, tox.WIDTH, tox.HEIGHT);
+  tox.controller = new tox.Controller();
+  tox.controller.init();
 };
 
 console.log(tox);
