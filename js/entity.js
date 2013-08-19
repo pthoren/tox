4233a1cf1378a7129ad9e82355d66adc9f34cc7f
Eugene <epaik@palantir.com>
2013-08-18 17:23:07 -0700

    Entity + EntityManager

---
+++ js/entity.js
@@ -0,0 +1,34 @@
+var tox = tox || {};
+
+tox.ENTITY_DEFAULT_RADIUS = 10;
+
+tox.Entity = function(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.dx = 0;
+  this.dy = 0;
+
+  this.radius = tox.ENTITY_DEFAULT_RADIUS;
+  this.die = false
+}
+
+tox.Entity.prototype.collideWithEntity = function(other) {
+
+};
+
+tox.Entity.prototype.update = function() {
+  // Override this
+};
+
+tox.Entity.prototype.draw = function(context) {
+	// Override this
+};
+
+tox.DEBUG_ENTITY_COLOR = 'red';
+tox.Entity.prototype.drawDebug = function(context) {
+  context.strokeStyle = tox.DEBUG_ENTITY_COLOR;
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+  context.stroke();
+};
---
+++ js/entitymanager.js
@@ -0,0 +1,45 @@
+var tox = tox || {};
+
+tox.EntityManager = function() {
+  this.entities = new Array();
+	this.drawDebug = true;
+};
+
+tox.EntityManager.prototype.update = function() {
+	for(var i = 0; i < this.entities.length; i++) {
+		this.entities[i].update();
+		for(var j = 0; j < this.entities.length; j++) {
+			if(i===j) {
+				continue;
+      }
+      // Inter-entity collision code may go here.
+		}
+	}
+	for(var i = 0; i < this.entities.length; i++) {
+		if(this.entities[i].die === true) {
+			delete(this.entities[i])
+    }
+	}
+	this.entities = this.entities.filter(function(){return true});
+};
+
+
+tox.EntityManager.prototype.draw = function(context) {
+	for(var i = 0; i < this.entities.length; i++) {
+		this.entities[i].draw(context);
+	}
+	if(this.drawDebug) {
+		for(var i = 0; i < this.entities.length; i++) {
+			this.entities[i].drawDebug(context);
+		}
+	}
+};
+
+tox.EntityManager.prototype.add = function(entity) {
+	this.entities.push(entity);
+};
+
+
+tox.EntityManager.prototype.clear = function(context) {
+	this.entities = new Array();
+}
